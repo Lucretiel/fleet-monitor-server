eeb21e2c58118dde4ce8b29e69ac194f2959cd89
Nathan West <Lucretiel@gmail.com>
2014-09-14 17:29:57 -0400

    Added various arguments and enhancements. Ready for testing

--- main.py
+++ main.py
@@ -31,24 +31,35 @@ class Machine(namedtuple('Machine', machine_fields)):
 
 
 @asyncio.coroutine
-def generic_scanner(Entity, frequency, update_callback):
+def generic_scanner(Entity, frequency, fleetctl_args, update_callback):
     '''
-    Repeatedly scan for entities every `frequency` seconds, and publish them to
-    the update_callback
+    Coroutine to perform the fleetctl polling. Repeatedly calls fleetctl every
+    `frequency` seconds, using `fleetctl_args`, which should have a path to
+    fleetctl as well as extra arguments, assembles the results into
+    json, and calls `update_callback` with it. Raises a RuntimeError if the
+    command fails.
     '''
 
+    # Get the entity type name (unit or machine)
     entity_type = Entity.__name__.lower()
+
+    # assemble the command
+    cmd = fleetctl_args + (
+        Entity.list_cmd, '-l', '-no-legend', '-fields',
+        ','.join(Entity._fields))
 
     while True:
         # Launch a fleetctl process
         process = yield from asyncio.create_subprocess_exec(
-            'fleetctl', Entity.list_cmd, '-l', '-no-legend',
-            '-fields', ','.join(Entity._fields),
+            *cmd,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.DEVNULL)
 
         # Retreive data from the process
         data, _ = yield from process.communicate()
+
+        if process.returncode:
+            raise RuntimeError('fleetctl failed too often')
 
         # Create an update json and send it to the callback
         update_callback(json.dumps(
@@ -106,26 +117,52 @@ class WebsocketHandler:
 
 
 def main(argv):
+    # ARGS
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
-
-    arg('-m', '--machine-freq', type=int, default=10, help=
-        "Frequency in seconds of list-machines polling")
-    arg('-f', '--unit-freq', type=int, default=2, help=
-        "Frequency in seconds of list-units polling")
+    arg('-m', '--machine-freq', type=int, default=10,
+        help="Frequency in seconds of list-machines polling")
+    arg('-f', '--unit-freq', type=int, default=2,
+        help="Frequency in seconds of list-units polling")
     arg('-p', '--port', type=int, default=8989)
+    arg('-c', '--fleetctl', default='fleetctl',
+        help="Path to the fleetctl binary")
 
+    endpoint_arg = parser.add_mutually_exclusive_group().add_argument
+    endpoint_arg('-e', '--endpoint', nargs=2)
+    endpoint_arg('-t', '--tunnel', nargs=2)
+
+    # PARSE ARGS
     args = parser.parse_args(argv)
+    if args.endpoint:
+        fleetctl_args = (
+            arg.fleetctl, '--endpoint', 'http://{}:{}'.format(args.endpoint))
+    elif args.tunnel:
+        fleetctl_args = (
+            arg.fleetctl, '--tunnel', '{}:{}'.format(args.tunnel))
+    else:
+        fleetctl_args = (arg.fleetctl,)
 
+    # Create socket manager
     socket_handler = WebsocketHandler()
 
+    # Spawn server
     loop = asyncio.get_event_loop()
 
-    loop.run_until_complete(asyncio.wait([
-        generic_scanner(Unit, args.unit_freq, socket_handler.update),
-        generic_scanner(Machine, args.machine_freq, socket_handler.update),
-        socket_handler.serve]))
-    loop.run_forever()
+    tasks = [
+        # Unit scanning coroutine
+        generic_scanner(
+            Unit, args.unit_freq, socket_handler.update, fleetctl_args),
+
+        # Machine scanning corouting
+        generic_scanner(
+            Machine, args.machine_freq, socket_handler.update, fleetctl_args),
+
+        # Websocket server
+        socket_handler.serve(args.port)]
+
+    loop.run_until_complete(
+        asyncio.wait(tasks, return_when=asyncio.FIRST_EXCEPTION))
 
 
 if __name__ == '__main__':
